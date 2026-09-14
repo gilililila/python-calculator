a620c2741d25c50fe89e3314a0cd82379f971cb3
Alice <1846888734@qq.com>
2026-09-14 17:12:16 +0800

    feat: add add() function with unit tests

---
+++ main.py
@@ -0,0 +1,12 @@
+from function.add import add
+
+
+def main():
+    print("Hello from alice-calculator!")
+    add_result = add(5, 3)
+    print(f"The result of addition is: {add_result}")
+
+
+
+if __name__ == "__main__":
+    main()
